fb5495761975a2486ac596953a7daa5c7b84d66d
himoto <himoto@protein.osaka-u.ac.jp>
2020-05-07 10:02:12 +0900

    Update name2idx

--- model/name2idx/parameters.py
+++ model/name2idx/parameters.py
@@ -214,9 +214,13 @@ param_names = [
     'kon95',
     'koff95',
     'kcat96',
-    #
-    'len_f_params'
 ]
 
-for idx,name in enumerate(param_names):
-    exec('%s=%d'%(name,idx))
+for idx, name in enumerate(param_names):
+    exec(
+        '{} = {:d}'.format(
+            name, idx
+        )
+    )
+
+len_f_params = len(param_names)
